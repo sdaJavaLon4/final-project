9b0c21c81e8a73ed96771b3cdaae3c474cb843df
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-11-28 10:39:29 +0100

    spring boot init

---
+++ src/main/java/pl/javalon4/finalproject/FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.javalon4.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectApplication.class, args);
+    }
+
+}

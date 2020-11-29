ae27843c0aca8c471bba0218266b6d1e77719fb2
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-11-29 13:28:51 +0100

    blueprint for users crud

--- src/main/java/pl/javalon4/finalproject/controller/AppUserController.java
+++ src/main/java/pl/javalon4/finalproject/controller/AppUserController.java
@@ -1,6 +1,8 @@
 package pl.javalon4.finalproject.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 import pl.javalon4.finalproject.dto.UserForm;
 import pl.javalon4.finalproject.service.AppUserService;
@@ -15,9 +17,28 @@ public class AppUserController {
         this.appUserService = appUserService;
     }
 
+    // CREATE
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void registerUser(@RequestBody UserForm userForm) {
         this.appUserService.registerUser(userForm);
+    }
+
+    // READ, get info about logged in user
+    @GetMapping
+    public AppUserDto getUser(@AuthenticationPrincipal User user) {
+        // TODO
+    }
+
+    // UPDATE, change password and email
+    @PatchMapping
+    public AppUserDto updateUser(@RequestBody UserUpdateForm updateForm, @AuthenticationPrincipal User user) {
+        // TODO:
+    }
+
+    // DELETE, delete logged in user (and invalidate session - later)
+    @DeleteMapping
+    public void deleteUser(@AuthenticationPrincipal User user) {
+        // TODO:
     }
 }

8de80b141e4f41e0479ea14bbfcce5e9d4ee8bc7
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-12-06 14:13:58 +0100

    chnage http 401 to 403

--- src/main/java/pl/javalon4/finalproject/security/SecurityConf.java
+++ src/main/java/pl/javalon4/finalproject/security/SecurityConf.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -30,6 +31,8 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.exceptionHandling()
+                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
         http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/hello").permitAll()
                 .antMatchers(HttpMethod.POST, "/user").permitAll().anyRequest().authenticated().and().httpBasic();
     }

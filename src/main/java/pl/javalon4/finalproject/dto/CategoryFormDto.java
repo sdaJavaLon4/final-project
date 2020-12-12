9fb4231e2323dd7af1d54c4a4f4531ec3ade7de9
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-12-12 14:32:39 +0100

    fix adding categories

--- src/main/java/pl/javalon4/finalproject/dto/CategoryFormDto.java
+++ src/main/java/pl/javalon4/finalproject/dto/CategoryFormDto.java
@@ -2,9 +2,11 @@ package pl.javalon4.finalproject.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryFormDto {
 
     private String name;

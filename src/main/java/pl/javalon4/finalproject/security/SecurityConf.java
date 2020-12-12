477bfb75aa7dddb53d7dfc4b87ef9d7c60cde706
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-12-12 14:23:36 +0100

    logout fix

--- src/main/java/pl/javalon4/finalproject/security/SecurityConf.java
+++ src/main/java/pl/javalon4/finalproject/security/SecurityConf.java
@@ -3,12 +3,14 @@ package pl.javalon4.finalproject.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -31,8 +33,8 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.exceptionHandling()
-                .authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+        http.exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint());
+        http.logout().logoutSuccessHandler((new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK)));
         http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/hello").permitAll()
                 .antMatchers(HttpMethod.POST, "/user").permitAll().anyRequest().authenticated().and().httpBasic();
     }
